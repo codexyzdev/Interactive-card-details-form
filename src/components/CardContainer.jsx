c2e0e82af6d212d29cfda558e96d0d4b226481cd
alejandrobaez <alejandrobaez938@gmail.com>
2025-02-10 15:01:47 -0400

    Actualiza el título de la página, mejora el manejo de estilos de fondo y refactoriza el componente de tarjetas

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import Cards from "./Cards";
 export default function CardContainer() {
   const [done, setDone] = useState(false);
   const {
@@ -19,35 +20,7 @@ export default function CardContainer() {
 
   return (
     <div className="grid-cols-2 lg:grid lg:min-h-screen lg:place-content-center">
-      <section className="relative h-[240px] w-[375px]">
-        <div className="card-back absolute right-6 top-8 w-64 drop-shadow-lg lg:-right-16 lg:top-60 lg:w-full">
-          <span className="absolute right-8 top-[57px] text-sm text-white lg:right-10 lg:top-[90px]">
-            {!watch("cvc") ? "000" : watch("cvc")}
-          </span>
-          <img src="/images/bg-card-back.png" alt="" />
-        </div>
-        <div className="card-front  absolute -bottom-5 left-6 w-64 drop-shadow-lg lg:bottom-0 lg:left-0  lg:top-0 lg:w-full">
-          <img src="/images/bg-card-front.png" alt="" />
-          <div className="absolute left-3 top-3 size-9 rounded-full bg-white lg:left-8 lg:top-4">
-            {" "}
-          </div>
-          <div className="absolute left-14 top-[22px] size-4 rounded-full border border-white lg:left-20 lg:top-7">
-            {" "}
-          </div>
-          <p className="absolute left-4 top-[70px] font-semibold tracking-widest text-white lg:left-8 lg:top-28 lg:text-xl">
-            {!watch("cardNumer")
-              ? "0000 0000 0000 0000"
-              : watch("cardNumer").replace(/([0-9]{4})/g, "$1 ")}
-          </p>
-          <span className="absolute left-48 top-[110px] text-xs uppercase tracking-widest text-white lg:left-72 lg:top-[165px] lg:text-base">
-            {!watch("mm") ? "00" : watch("mm")}/
-            {!watch("yy") ? "00" : watch("yy")}
-          </span>
-          <p className="absolute left-4 top-[110px] flex gap-16 text-xs uppercase tracking-widest text-white lg:left-8 lg:top-[165px] lg:text-base">
-            <span>{!watch("name") ? "  Jane Appleseed" : watch("name")}</span>
-          </p>
-        </div>
-      </section>
+      <Cards watch={watch} />
 
       <form
         className="relative mt-20 px-7 lg:w-96"
@@ -82,6 +55,7 @@ export default function CardContainer() {
           <input
             type="text"
             placeholder="e.g. 1234 5678 9123 0000"
+            max="16"
             {...register("cardNumer", {
               required: "Can't be blank",
               validate: (value) => {
@@ -100,7 +74,7 @@ export default function CardContainer() {
             {errors.cardNumer?.message}
           </p>
         </div>
-        <div className="mt-6  flex gap-3">
+        <div className="mt-6  flex gap-2">
           <div className="flex flex-col">
             <label
               className="mb-2 text-xs font-medium uppercase text-VeryDarkViolet"
@@ -108,7 +82,7 @@ export default function CardContainer() {
             >
               Exp. Date (MM/YY)
             </label>
-            <div className="relative flex gap-1">
+            <div className="relative flex gap-2">
               <input
                 type="text"
                 placeholder="MM"
